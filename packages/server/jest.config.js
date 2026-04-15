d38c5c6b4f69fc23edebcf628ba99ddff1c3d262
dafmulder <dafmulder@gmail.com>
2026-04-15 10:15:30 +0200

    fix: resolve test typescript errors in server and test packages

--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -4,4 +4,10 @@ module.exports = {
 	testMatch: ["**/__tests__/**/*.test.ts"],
 	verbose: true,
 	testTimeout: 10000,
+	transform: {
+		"^.+\\.ts$": [
+			"ts-jest",
+			{ diagnostics: false },
+		],
+	},
 };
